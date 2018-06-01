1ef0c65396a9ccca1a849a32a677f7f5df0704e7
liheisuper <13718542261@163.com>
2018-06-01 18:30:48 +0800

    liuhualei6.1

--- Screendis/javascript/mainjs.js
+++ Screendis/javascript/mainjs.js
@@ -1,7 +1,22 @@
 // JavaScript Document
 //接口公共地址
-urlmose= 'http://192.168.3.105:8000';
- //urlmose= 'http://www.lisuper.cn';
+//urlmose= 'http://192.168.3.105:8000';
+ urlmose= 'http://www.lisuper.cn';
+ (function(){
+	if(window.attachEvent){
+    	window.attachEvent("load", asyncLoad);
+    }else{
+    	window.addEventListener("load", asyncLoad);
+    }
+    var asyncLoad = function(){
+    	var ga = document.createElement('script'); 
+        ga.type = 'text/javascript'; 
+        ga.async = true; 
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; 
+        var s = document.getElementsByTagName('script')[0]; 
+        s.parentNode.insertBefore(ga, s);
+    };
+})
 /*
 右上角获取当前时间
 */
@@ -142,12 +157,13 @@ dv1总指数
 */
 $(document).ready(function() {
 	$.ajax({
-	      url:urlmose+'/index.php/index/index/test',
+	      url:'http://www.lisuper.cn/index.php/index/index/test',
 	      type:'post',
 	      dataType:'json',
 	      data:{},
 	      async:false,
 	      success:function(data){
+	      	// alert(data);
 	      	$("#month").html(data.month+"月产新品种");
 	      	$("#left-tx1").html(data.present); 
 	      	$("#left-tx2").html(data.next_present); 			 
@@ -167,12 +183,13 @@ $(document).ready(function() {
 /*示范区*/
 $(document).ready(function() {
 $.ajax({
-  url:urlmose+'/index.php/index/Picture/demonstration',
-  type:'get',
+  url:'http://www.lisuper.cn/index.php/index/Picture/demonstration',
+  type:'post',
   dataType:'json',
   data:{},
   async:false,
   success:function(datas){
+  	alert(datas.error_code);
   	var vendorJson = eval(datas.date);
   	var html ="";
     for(var i=0; i<vendorJson.length; i++)
@@ -202,7 +219,7 @@ $.ajax({
 	   };
   },
   error:function(data){
-
+  	// alert(123)
   }
 });
 
@@ -258,84 +275,165 @@ $.ajax({
  */
 /*图表*/
 $(document).ready(function() {
-	   var chart= {  		  
-		  backgroundColor: 'rgba(0,0,0,0)' ,
-		   
-	   }; 
-	   var title = {
-		  text: null   
-	   };
-	   var subtitle = {
-		  text: null
-	   };
-	   var xAxis = {
-		   /*labels:{ 
-			step:12
-		  },*/
-		  categories: ['2015年1月','2015年2月','2015年3月','2015年4月','2015年5月','2015年6月','2015年7月','2015年8月','2015年9月','2015年10月',
-		               '2015年11月','2015年12月','2016年1月','2016年2月','2016年3月','2016年4月','2016年5月','2016年6月','2016年7月','2016年8月',
-		               '2016年9月','2016年10月','2016年11月','2016年12月','2017年1月','2017年2月','2017年3月','2017年4月','2017年5月','2017年6月',
-		               '2017年7月','2017年8月','2017年9月','2017年10月','2017年11月','2017年12月','2018年1月','2018年2月','2018年3月','2018年4月',
-		               '2018年5月','2018年6月','2018年7月','2018年8月','2018年9月','2018年10月','2018年11月','2018年12月'],
-		  tickInterval:12,
-		  gridLineColor: '#6c6a91',//网格线颜色
-		  color:'#6c6a91'
-		  /*labels: {
-                rotation: -15//让标签旋转-45°
-            }*/
-	        
-	   };
-	   var yAxis = {
-		  title: {
-			 text: null
-		  },
-		  gridLineColor: '#6c6a91',//纵向网格线颜色
-		  lineWidth:1,  //Y轴默认不显示Y轴线，添加一个轴线的宽度就可以显示出来
-		  color:'#6c6a91',
-　　      tickPixelInterval:10 , //自行调整Y轴刻度的间距 
-		  labels: {//y轴刻度文字标签  
-			formatter: function () {  
-				return this.value + 'k';//y轴加上%  
-			}  
-		  },  
-		 
-	   };   
-	
-	   var tooltip = {
-		  valueSuffix: 'k'
-	   }
-	
-	   var legend = {
-		  enabled: false
-	   };
 	   
-	   var credits = {  
-		enabled: false     //不显示LOGO 
-	   };
-	
-	   var series =  [
-		  {
-			 name: '红花 荷花池药市',
-			 data: [83,83,83,82,78,78,80,80,88,95,100,100,84,89,83,83,86,86,90,90,115,115,110,110,105,105,96,96,89,89,89,98,98,100,100,
-			        86,86,96,96,90,83,83,83,82,78,78,80,80],
-			 color: '#ffb400'
-		  }
-	   ];
-	   
-	   var json = {};
-	   json.chart = chart;
-	   json.title = title;
-	   json.subtitle = subtitle;
-	   json.xAxis = xAxis;
-	   json.yAxis = yAxis;
-	   json.tooltip = tooltip;
-	   json.legend = legend;
-	   json.credits = credits;
-	   json.series = series;	
-	   $('#middle-tu').highcharts(json);
 	});
 
 /*列表*/
+
+ for (var a = 1; a < 100; a++) { 	
+    (function(i) {
+        setTimeout(function() {
+        	var chart= {  		  
+		  backgroundColor: 'rgba(0,0,0,0)' ,
+		   
+		   }; 
+		   var title = {
+			  text: null   
+		   };
+		   var subtitle = {
+			  text: null
+		   };
+		   var xAxis = {
+			   /*labels:{ 
+				step:12
+			  },*/
+			  categories: ['2015年1月','2015年2月','2015年3月','2015年4月','2015年5月','2015年6月','2015年7月','2015年8月','2015年9月','2015年10月',
+			               '2015年11月','2015年12月','2016年1月','2016年2月','2016年3月','2016年4月','2016年5月','2016年6月','2016年7月','2016年8月',
+			               '2016年9月','2016年10月','2016年11月','2016年12月','2017年1月','2017年2月','2017年3月','2017年4月','2017年5月','2017年6月',
+			               '2017年7月','2017年8月','2017年9月','2017年10月','2017年11月','2017年12月','2018年1月','2018年2月','2018年3月','2018年4月',
+			               '2018年5月','2018年6月','2018年7月','2018年8月','2018年9月','2018年10月','2018年11月','2018年12月'],
+			  tickInterval:12,
+			  gridLineColor: '#6c6a91',//网格线颜色
+			  color:'#6c6a91'
+			  /*labels: {
+	                rotation: -15//让标签旋转-45°
+	            }*/
+		        
+		   };
+		   var yAxis = {
+			  title: {
+				 text: null
+			  },
+			  gridLineColor: '#6c6a91',//纵向网格线颜色
+			  lineWidth:1,  //Y轴默认不显示Y轴线，添加一个轴线的宽度就可以显示出来
+			  color:'#6c6a91',
+	　　      tickPixelInterval:10 , //自行调整Y轴刻度的间距 
+			  labels: {//y轴刻度文字标签  
+				formatter: function () {  
+					return this.value + 'k';//y轴加上%  
+				}  
+			  },  
+			 
+		   };   
+		
+		   var tooltip = {
+			  valueSuffix: 'k'
+		   }
+		
+		   var legend = {
+			  enabled: false
+		   };
+		   
+		   var credits = {  
+			enabled: false     //不显示LOGO 
+		   };
+		
+		   var series =  [
+			  {
+				 name: '红花 荷花池药市',
+				 data:[i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,
+				        i,i,i,i,i,i,i,i,i,i,i,i,i],
+				 /*data: [83,83,83,82,78,78,80,80,88,95,100,100,84,89,83,83,86,86,90,90,115,115,110,110,105,105,96,96,89,89,89,98,98,100,100,
+				        86,86,96,96,90,83,83,83,82,78,78,80,80],*/
+				 color: '#ffb400'
+			  }
+		   ];
+		   
+		   var json = {};
+		   json.chart = chart;
+		   json.title = title;
+		   json.subtitle = subtitle;
+		   json.xAxis = xAxis;
+		   json.yAxis = yAxis;
+		   json.tooltip = tooltip;
+		   json.legend = legend;
+		   json.credits = credits;
+		   json.series = series;	
+		   $('#middle-tu').highcharts(json);
+
+
+            var html = "<li><span>&nbsp;</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span>"
+	            +"<span>7</span><span>8</span><span>9</span><span>10</span><span>11</span><span>12</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>";
+            $("#middle-b-list").html(html);
+        }, i*3000); //.....
+    })(a)
+  }
+	
+	/*for(var i=0; i<100; i++)
+	{
+	  html += "<li><span>&nbsp;</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span>"
+	            +"<span>7</span><span>8</span><span>9</span><span>10</span><span>11</span><span>12</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>"
+	            +"<li><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span>"
+	            +"<span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span><span>"+i+"</span></li>";
+	}*/
+	
+
+/*$(document).ready(function() {
+$.ajax({
+  url:urlmose+'/index.php/index/picture/bazaar',
+  type:'get',
+  dataType:'json',
+  data:{},
+  async:false,
+  success:function(datas){
+  	var vendorJson = eval(datas.);
+  	var html ="";
+    for(var i=0; i<vendorJson.length; i++)
+    {
+	  html += "<li><span>&nbsp;</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span>"
+                +"<span>7</span><span>8</span><span>9</span><span>10</span><span>11</span><span>12</span></li>"
+                +"<li><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span>"
+                +"<span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span></li>"
+                +"<li><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span>"
+                +"<span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span></li>"
+                +"<li><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span>"
+                +"<span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span></li>"
+                +"<li><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span>"
+                +"<span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span><span>"++"</span></li>";
+    }
+	  $("#middle-b-list").html(html);
+	 
+	  }
+})
+})*/
+/* var doscroll = function(){
+      var $parent = $('.js-slide-list');
+      var $first = $parent.find('li:first');
+      var height = $first.height();
+      $first.animate({
+     	//切换时动画持续时间
+         marginTop: -height + 'px'
+         },2000, function() {
+         $first.css('marginTop', 0).appendTo($parent);
+      });    
+	  };
+	 //切换中动画间隔时间
+	 setInterval(function(){doscroll()},15000);*/
+
 
 
 /*
